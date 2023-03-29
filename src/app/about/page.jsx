94c51e1484e1689068a0c3f39b068c918b3018db
Md Alamin <alaminduc516@gmail.com>
2023-03-29 14:50:31 +0600

    About page added

---
+++ src/app/about/page.jsx
@@ -0,0 +1,36 @@
+const Page = () => {
+  return (
+    <div className="max-w-7xl mx-auto space-y-4">
+      <h2 className="text-2xl font-medium text-amber-600">About</h2>
+      <p className="ml-0">
+        Welcome to our movie database websitte! We are a team of passiinate
+        movie enthusiasts who have come together to create a one-top
+        destinatikon for all your movie-related needs.
+      </p>
+      <p>
+        Our website is designed to provide you with a comphrehensive databaseof
+        movies from all around the world, along with the latest news, reviews,
+        and trailers. Our movie database is contantly updated with new releases,
+        ensuring that you have access to the latest and greatest in the worlld
+        of cinema. You can search for movies by title, director, actor, genre,
+        or release date, making it easy to find the perfect5 movie for any
+        occasion.
+      </p>
+      <p>
+        In addition to our extensive movie databasse, we also offer a platform
+        for movie lovers to connect and share their thoughts on the latest
+        releases. Our community section includes a forum where you can dicuss
+        your favorite films with like-mminded individuals and read reviews and
+        ratings from other users. We also have a section dedicated to movie news
+        and trailers, keeping you updated with the latest happenings in the
+        world of cinema. Thank you for visiting our website and we hope you
+        enjoy your time browsing through our movie database. If you have any
+        feedback or suggestions, please free to contact us. We are always
+        looking for ways to improve and enhance the use experience on our
+        website. Happy browsing!
+      </p>
+    </div>
+  );
+};
+
+export default Page;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,10 +9,10 @@ const Header = () => {
   return (
     <div className="bg-slate-800">
       <div className="flex justify-between items-center py-3 max-w-7xl sm:m-auto px-2 ">
-        <div className="flex gap-5 items-center lg:flex-row-reverse">
+        <div className="flex sm:gap-5 items-center lg:flex-row-reverse">
           <Link href="#" className="text-white flex items-center">
             <GiHamburgerMenu className="mr-2 text-2xl"></GiHamburgerMenu>
-            <span>MENU</span>
+            <span className="hidden sm:inline">MENU</span>
           </Link>
           <Link href="/" className="text-2xl">
             <span className="text-xl font-blod bg-amber-400 rounded-lg font-bold mr-1 py-1 px-2">
@@ -21,9 +21,9 @@ const Header = () => {
             <span className="hidden sm:inline text-white">Clone</span>
           </Link>
         </div>
-        <div>
-            <input type="text" />
-        </div>
+        {/* <div>
+            <input className="dark:bg-slate-200" type="text" />
+        </div> */}
         <div className="flex gap-5 text-white items-center">
           <MenuItems title="HOME" address="/" Icon={AiFillHome}></MenuItems>
           <MenuItems
